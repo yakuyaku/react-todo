663cef647a239eec8b16904cb92eea34bb8b6465
yakuyaku <yakuyauk@gmail.com>
2025-12-01 11:07:50 +0900

    goog

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,6 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000,
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: 'esbuild',
   },
 });
